7b9fdb56498b17da4ed1b10a81c696a89782a4f8
Hanai <happy926926@gmail.com>
2025-06-06 23:07:11 +0100

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db' + os.path.join(basedir, 'app.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = 'your-secret-key'
