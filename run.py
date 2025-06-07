aee207cfcb14ab2b3a4631e8cf12c5330ce1edcc
Hanai <happy926926@gmail.com>
2025-06-07 01:37:24 +0100

    Update run.py

--- run.py
+++ run.py
@@ -1,12 +1,12 @@
 from app import create_app, db
 from app.models import User
 
-app = create_app()
-
-with app.app_context():
-    db.create_all()
 
     
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app = create_app()
+    app.run(host="0.0.0.0", port=port)
+
+   # app.run(host='0.0.0.0', port=5000)
 
