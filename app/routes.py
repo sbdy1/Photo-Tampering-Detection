6b125a8ef01017d4f1037674b98ec22612f6e6f7
Hanai <happy926926@gmail.com>
2025-06-07 19:09:06 +0100

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, session, flash, redirect
+from flask import Blueprint, render_template, session, flash, redirect, url_for
 from flask_login import login_required, current_user
 
 main_bp = Blueprint('main', __name__)
