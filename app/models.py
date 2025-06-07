370a7aa1f1fd523b4b4dfc4ebe2ce70f32b642c0
Hanai <happy926926@gmail.com>
2025-06-07 13:39:51 +0100

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,6 +7,8 @@ from app import db, bcrypt
 #db = SQLAlchemy()
 
 class User(UserMixin, db.Model):
+    __tablename__ = "user"
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(150), nullable=False, unique=True)
     email = db.Column(db.String(150), unique=True)
