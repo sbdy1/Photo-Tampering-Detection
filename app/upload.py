c218bde3834a2895f907d19e8cf6dd8b221df016
Hanai <happy926926@gmail.com>
2025-06-12 11:53:09 +0100

    Update upload.py

--- app/upload.py
+++ app/upload.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, request, jsonify, current_app
+from flask import Blueprint, request, jsonify, current_app, render_template, send_from_directory
 from werkzeug.utils import secure_filename
 import os
 from .utils import (
