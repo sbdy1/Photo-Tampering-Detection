4967c8460d2bd6d98d99d61d5b88ecba92ceb0ee
Hanai <happy926926@gmail.com>
2025-06-18 21:05:03 +0100

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///' + os.path.join(basedir, 'app.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-SECRET_KEY = os.getenv('SECRET_KEY', 'your-default-secret-key')
-
